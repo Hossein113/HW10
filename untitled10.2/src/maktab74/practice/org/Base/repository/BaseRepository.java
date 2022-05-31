f3763963e0a33b092118acffd8f277dc2cc9c0fd
Hossein113 <hos_m18@yahoo.com>
2022-06-01 01:51:31 +0430

    added BaseEntityRepositort

---
+++ untitled10.2/src/maktab74/practice/org/Base/repository/BaseRepository.java
@@ -0,0 +1,10 @@
+package maktab74.practice.org.Base.repository;
+
+import maktab74.practice.org.Base.domain.BaseEntity;
+
+import java.io.Serializable;
+
+public interface BaseRepository<E extends BaseEntity<ID>, ID extends Serializable> {
+
+
+}
