5f36000e0e3dc111420c9f514b81a1eecd09c324
Hossein113 <hos_m18@yahoo.com>
2022-06-01 01:51:56 +0430

    added BaseService

---
+++ untitled10.2/src/maktab74/practice/org/Base/service/BaseService.java
@@ -0,0 +1,9 @@
+package maktab74.practice.org.Base.service;
+
+import maktab74.practice.org.Base.domain.BaseEntity;
+
+import java.io.Serializable;
+
+public interface BaseService<E extends BaseEntity<ID>, ID extends Serializable> {
+
+}
