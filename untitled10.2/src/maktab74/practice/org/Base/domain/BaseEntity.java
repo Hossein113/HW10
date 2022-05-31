4fa58ec4cde9259575bd24c9587f352af960c619
Hossein113 <hos_m18@yahoo.com>
2022-06-01 01:51:08 +0430

    added BaseEntity

---
+++ untitled10.2/src/maktab74/practice/org/Base/domain/BaseEntity.java
@@ -0,0 +1,30 @@
+package maktab74.practice.org.Base.domain;
+
+import java.io.Serializable;
+
+public class BaseEntity<ID extends Serializable> implements Serializable {
+
+    private ID id;
+
+    public BaseEntity() {
+    }
+
+    public BaseEntity(ID id) {
+        this.id = id;
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
