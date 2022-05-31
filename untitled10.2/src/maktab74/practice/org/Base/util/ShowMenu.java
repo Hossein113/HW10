c15868b8ddad2d3706bd0017184d742aa451e667
Hossein113 <hos_m18@yahoo.com>
2022-06-01 01:53:56 +0430

    added ShowMenu

---
+++ untitled10.2/src/maktab74/practice/org/Base/util/ShowMenu.java
@@ -0,0 +1,20 @@
+package maktab74.practice.org.Base.util;
+
+public class ShowMenu {
+
+    public static void showName() {
+        System.out.println("Enter Name :");
+    }
+
+    public static void showAmount() {
+        System.out.println("Enter Amount :");
+    }
+
+    public static void showService() {
+        System.out.println("Enter Service :");
+    }
+
+    public static void showCountinu() {
+        System.out.println("countinu ??? n / y :");
+    }
+}
