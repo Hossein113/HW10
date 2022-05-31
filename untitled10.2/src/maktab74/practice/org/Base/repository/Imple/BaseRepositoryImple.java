f568323e99c503d0e5f3677307a2491ccbb3150b
Hossein113 <hos_m18@yahoo.com>
2022-06-01 01:51:43 +0430

    added BaseEntityRepositorImple

---
+++ untitled10.2/src/maktab74/practice/org/Base/repository/Imple/BaseRepositoryImple.java
@@ -0,0 +1,11 @@
+package maktab74.practice.org.Base.repository.Imple;
+
+import maktab74.practice.org.Base.domain.BaseEntity;
+import maktab74.practice.org.Base.repository.BaseRepository;
+
+import java.io.Serializable;
+
+public class BaseRepositoryImple<E extends BaseEntity<ID>, ID extends Serializable> implements BaseRepository<E, ID> {
+
+
+}
